7677d3716b126e4c6b79403876c35a0caf71e4a5
awishkara15 <awishkara15@gmail.com>
2025-06-05 23:55:30 +0530

    ME_QA_XSTACKOVERFLOW_QUERY_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,9 +1,12 @@
 package demo;
 
+import java.security.Key;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -35,7 +38,7 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
@@ -50,8 +53,142 @@ public class TestCases {
 
      public void testCase01(){
      System.out.println("Start Test case: testCase01");
-     driver.get("https:www.google.com");
-     System.out.println("end Test case: testCase02");
+     driver.get("https://stackoverflow.com/");
+
+     String url = driver.getCurrentUrl();
+     if(url.contains("stackoverflow.")){
+        System.out.println("Testcase01 : Pass");
      }
+     else{
+        System.out.println("Testcase01 : Fail");
+     }
+
+     System.out.println("end Test case: testCase01");
+     }
+
+     public void testCase02(){
+        System.out.println("Start Test case: testCase02");
+        driver.get("https://stackoverflow.com/");
+
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys("Python list comprehension");
+        search.sendKeys(Keys.ENTER);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        WebElement result = driver.findElement(By.xpath("//div[contains(@class,'fs-caption fc-black')]/div[1]"));
+        String resultText = result.getText();
+
+        System.out.println(resultText);
+   
+        
+        if(resultText.contains("python list comprehension")){
+           System.out.println("Testcase02 : Pass");
+        }
+        else{
+           System.out.println("Testcase02 : Fail");
+        }
+   
+        System.out.println("end Test case: testCase02");
+        }
+
+
+        public void testCase03(){
+            System.out.println("Start Test case: testCase03");
+            driver.get("https://stackoverflow.com/");
+
+            try {
+               Thread.sleep(6000);
+            } catch (InterruptedException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+            }
+
+            WebElement tagButton = driver.findElement(By.xpath("//li[@class='ps-relative'][2]/a"));
+            tagButton.click();
+            try {
+               Thread.sleep(5000);
+            } catch (InterruptedException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+            }
+
+            WebElement tagSearch = driver.findElement(By.id("tagfilter"));
+            tagSearch.sendKeys("javascript");
+            tagSearch.sendKeys(Keys.ENTER);
+
+            try {
+               Thread.sleep(3000);
+            } catch (InterruptedException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+            }
+
+            List<WebElement> tags = driver.findElements(By.xpath("//div[@class='flex--item']/a"));
+
+            for(WebElement tag : tags){
+               if(tag.getText().equals("javascript")){
+                  tag.click();
+                  break;
+               }
+            }
+
+            try {
+               Thread.sleep(3000);
+            } catch (InterruptedException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+            }
+
+
+            List<WebElement> questions = driver.findElements(By.xpath("//h3[@class='s-post-summary--content-title']/a"));
+
+            boolean flag = false;
+             
+            for(WebElement question : questions){
+               if(question.getText().toLowerCase().contains("javascript")){
+                  flag=true;
+                  break;
+               }
+            }
+
+            if(flag){
+               System.out.println("Testcase03 : Pass");
+            }
+            else{
+               System.out.println("Testcase03 : Fail");
+            }
+        }
+
+        public void testCase04(){
+            System.out.println("Start Test case: testCase04");
+            driver.get("https://stackoverflow.com/");
+
+            WebElement more = driver.findElement(By.xpath("//span[@data-text='More']"));
+            more.click();
+
+            WebElement score = driver.findElement(By.xpath("//li[@role='menuitem'][4]/a"));
+            score.click();
+
+            List<WebElement> votes = driver.findElements(By.xpath("//div[@class='s-post-summary--stats-item s-post-summary--stats-item__emphasized']/span[1]"));
+
+            int firstVote = Integer.parseInt(votes.get(0).getText());
+            int secondVote = Integer.parseInt(votes.get(1).getText());
+
+            if(firstVote>secondVote){
+               System.out.println("Testcase04 : Pass");
+            }
+            else{
+               System.out.println("Testcase04 : Fail");
+            }
+       
+            System.out.println("end Test case: testCase04");
+        }
+
 
 }
